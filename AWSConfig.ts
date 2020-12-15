17150a01284fe942e560c68f1e92917a61363cf2
Malcolm Navarro <mnavarro@templuminc.com>
2020-12-14 19:30:52 -0600

    Added types to AWS config exports

--- AWSConfig.ts
+++ AWSConfig.ts
@@ -1,10 +1,12 @@
-const sqsConfig = {
+import { ConfigurationOptions } from "aws-sdk";
+
+const sqsConfig: ConfigurationOptions = {
   region: "us-east-1",
   accessKeyId: "N/A",
   secretAccessKey: "N/A"
 }
 
-const sqsUrl = "http://localhost:9324";
-const sqsQueue = `${sqsUrl}/queue/main-queue`;
+const sqsUrl: string = "http://localhost:9324";
+const sqsQueue: string = `${sqsUrl}/queue/main-queue`;
 
 export { sqsConfig, sqsUrl, sqsQueue };
